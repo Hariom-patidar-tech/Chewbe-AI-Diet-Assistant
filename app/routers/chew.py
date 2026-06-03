f6a6837d6ee0a006a94799308316b025c5666733
Hariom Patidar <hariompatidar977053@gmail.com>
2026-06-03 13:04:22 +0530

    update chew

--- app/routers/chew.py
+++ app/routers/chew.py
@@ -7,15 +7,18 @@ router = APIRouter()
 
 @router.post("/start")
 def start_chew_process(db: Session = Depends(get_db)):
-    # Ab service ko sirf database session chahiye
+    # Service call
     data = detect_chews(db=db)
     
+    # Error checking
     if "error" in data:
         raise HTTPException(status_code=500, detail=data["error"])
     
+    # Sahi response return karo
     return {
         "status": "success",
-        "total_chews": data["total_chews"],
-        "active_chewing_seconds": data.get("active_chew_duration", 0),
-        "message": "Session saved successfully !"
+        "chew_count": data["chew_count"],
+        "bite_count": data["bite_count"],
+        "active_chewing_seconds": data["active_chew_duration"],
+        "message": "Session saved successfully!"
     }
