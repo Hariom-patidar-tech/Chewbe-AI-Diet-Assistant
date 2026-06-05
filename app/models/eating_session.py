bc529a3247fd0848bd501c0d6adc95b05fb25073
Hariom Patidar <hariompatidar977053@gmail.com>
2026-06-05 16:42:06 +0530

    update

--- app/models/eating_session.py
+++ app/models/eating_session.py
@@ -1,40 +1,14 @@
-from sqlalchemy import Column, Integer, Float, DateTime
-from datetime import datetime
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
 from app.db.database import Base
+from datetime import datetime
 
 class EatingSession(Base):
     __tablename__ = "eating_sessions"
-
-    id = Column(
-        Integer, 
-        primary_key=True, 
-        index=True
-    )
-
-    # user_id aur meal_id yahan se bilkul hata diye gaye hain
-
-    chew_count = Column(
-        Integer, 
-        default=0
-    )
-    
-    bite_count = Column(
-       Integer,
-        default = 0  
-    )
-    
-    eating_speed = Column(
-        Float, 
-        default=0
-    )
-
-    started_at = Column(
-        DateTime, 
-        default=datetime.utcnow
-    )
-
-    ended_at = Column(
-        DateTime, 
-        default=datetime.utcnow,
-        onupdate=datetime.utcnow
-    )
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False) # Zaroori
+    meal_id = Column(Integer, ForeignKey("meals.id"), nullable=True)
+    chew_count = Column(Integer, default=0)
+    bite_count = Column(Integer, default=0)
+    eating_speed = Column(Float, default=0)
+    started_at = Column(DateTime, default=datetime.utcnow)
+    ended_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
